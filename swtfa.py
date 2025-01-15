c7106bf36ed01c36342027cecbf90e826ec295d8
Austin Jones <austin.jones@uster.com>
2025-01-15 16:24:12 -0500

    make it cooler

--- swtfa.py
+++ swtfa.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('sweep_results.csv', encoding='unicode_escape')
+data = pd.read_csv('new_sweep_results.csv', encoding='unicode_escape')
 
 last_row = data.iloc[-1]
 
 max_i = int(last_row['i'] + 1)
 max_j = int(last_row['j'] + 1)
+range_i = max_i / 2
+range_j = max_j / 2
 
 a = np.zeros((max_i, max_j))
 
@@ -20,17 +22,19 @@ for i in range(max_i):
         a[i][j] = row['err']
 
 f = plt.figure()
-# plt.imshow(a, cmap='hot', interpolation='nearest')
-plt.colorbar(plt.pcolor(a))
+plt.imshow(a, cmap='jet', origin='lower')
+plt.colorbar()
 
 plt.title("error for data alignment params for adjustments from len = 16.9 and sfc = 33%")
 
-plt.plot(10, 10, 'ro')
+plt.plot(range_i, range_j, 'ro')
 
-plt.ylabel("len")
-plt.yticks([v for v in range(max_i)], [int(16.9 * (1 + (v - 10) / 20)) for v in range(max_i)])
+plt.ylabel("len (mm)")
+plt.yticks([v for v in range(max_i)], [round(16.9 * (1 + (v - range_i) / max_i), 2) for v in range(max_i)])
+plt.tick_params(axis='y', labelsize=3)
 
-plt.xlabel("sfc")
-plt.xticks([v for v in range(max_j)], [int(34 * (1 + (v - 10) / 20)) for v in range(max_i)])
+plt.xlabel("sfc (%)")
+plt.xticks([v for v in range(max_j)], [round(34 * (1 + (v - range_j) / max_j)) for v in range(max_j)])
+plt.tick_params(axis='x', labelsize=3)
 
 f.savefig("foo.pdf", bbox_inches='tight')
